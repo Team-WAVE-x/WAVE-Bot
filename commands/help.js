28f7bbd3bcc6d42b6d861d32c356c9bd334676ca
1-EXON <exon.indiv@gmail.com>
2020-06-03 21:27:51 +0900

    Change Help

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,8 @@ exports.run = (client, message, args) => {
     message.channel.send(
         new Discord.MessageEmbed()
             .setTitle("Help")
-            .addField(`**${config.prefix}ping**`, `핑을 알려줍니다.`, true)
+            .addField(`**${config.prefix}ping**`, `핑을 알려줍니다.`, false)
+            .addField(`**${config.prefix}tendinous**`, `개발자들에게 건의해 보세요.`, false)
     );
 }
 
--- commands/건의.js
+++ commands/건의.js
@@ -6,7 +6,7 @@ exports.run = (client, message, args) => {
             .setTitle("개발자들아 건의 날라옴")
             .addField(`건의자 : ${message.author.tag}`, `${args}`, true)
     );
-} // 마소 멍청이
+}
 
 module.exports.help = {
     name: "tendinous",
