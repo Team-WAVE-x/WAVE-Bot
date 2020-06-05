82df41a91d38d8ef9ee76968f37e06eb7880a3a4
1-EXON <exon.indiv@gmail.com>
2020-06-05 11:16:27 +0900

    Ban List

--- commands/건의.js
+++ commands/건의.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) => {
     message.channel.send(
         new Discord.MessageEmbed()
             .setTitle("개발자들아 건의 날라옴")
-            .addField(`건의자 : ${message.author.tag}`, `${args}`, true)
+            //.addField(`건의자 : ${message.author.tag}`, `${args}`, true)
     );
 }
 
--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 module.exports = {
   token: "NzE0MzcxNzIzMjg1MjMzNjc1.XsuARg.gQJP892nrRUfP87HD-4og9Bfvro",
   prefix: "%",
-  developers: [
+  developers: [ //Team WAVE
     "495776817937121301", // EXON
     "472652198388695040", // HOOSS
-    "515833390524530688" // Kate Line
+    "515833390524530688", // Kate Lin
+    "562921530376323081" // MILCH4089 코드 수정 안하면 엑슨됨 ㅅㄱ
   ],
 };
